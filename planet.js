bd93b39bfa4526d9dde792dd47f619bd115f5a28
Ananya Pamal <niyapamal@gmail.com>
2025-09-29 16:15:49 -0400

    fixing planet

--- planet.js
+++ planet.js
@@ -3,6 +3,7 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer({ canvas: document.getElementById('planetCanvas'), alpha: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setClearColor(0x000000, 0); // transparent black
 
 // Lighting (for realism)
 const ambientLight = new THREE.AmbientLight(0x888888);
@@ -17,7 +18,7 @@ const textureLoader = new THREE.TextureLoader();
 const marsTexture = textureLoader.load('images/2k_mars.jpg');
 
 const geometry = new THREE.SphereGeometry(2, 64, 64);
-const material = new THREE.MeshPhongMaterial({
+const material = new THREE.MeshBasicMaterial({
   map: marsTexture
 });
 
@@ -25,8 +26,6 @@ const planet = new THREE.Mesh(geometry, material);
 planet.position.set(0, 0, 0);   // center planet
 camera.position.set(0, 0, 6);   // pull camera back so planet is visible
 scene.add(planet);
-
-camera.position.z = 5;
 
 function animate() {
   requestAnimationFrame(animate);
